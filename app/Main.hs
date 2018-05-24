f10c42830987c75ba029a55ee50102cbb19ebc96
Aearnus <tslimkemann42@gmail.com>
2018-05-24 02:09:54 -0700

    Actual use

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
 import SixtyFiveOhTwo.Instruction
+import Control.Monad.State
+
+accumulatorLoadNStore :: Instruction
+accumulatorLoadNStore = do
+    lda (Immediate 0x10)
+    sta (Absolute 0x0200)
+    rts (Implied)
+
+myProgram :: Instruction
+myProgram = do
+    define "accumulatorLoadNStore" accumulatorLoadNStore
+    call "accumulatorLoadNStore"
 
 main :: IO ()
-main = putStrLn "Compiled"
+main = print $ execState myProgram emptyState
